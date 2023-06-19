8ab39e51b1789ba77dd1a783c1894f644a4752bd
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-06-19 16:54:01 +0530

    task done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import Stars from "./Stars";
 
 const ProductCard = ({title,desscription}) => {
   return (
-    <div className="flex max-w-[650px] p-4 bg-white radi justify-between gap-12 border rounded-lg shadow-lg">
+    <div className="flex max-w-[650px] p-4 bg-white radi justify-between gap-12 border rounded-lg drop-shadow-xl">
       <div className="flex flex-col justify-center gap-2 text-sm">
         <p>Sula Vineyards</p>
         <h2 className="font-bold text-pink-700 capitalize">{title}</h2>
@@ -19,7 +19,7 @@ const ProductCard = ({title,desscription}) => {
         }
         width={"250"}
         height={"250"}
-        className="aspect-square"
+        className="rounded-md aspect-square"
       />
     </div>
   );
